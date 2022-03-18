cd335a3a3b70de6c0bdaa3565ba96ec7073969b9
Reza Davari <rezazzr@hotmail.com>
2022-03-18 15:09:13 -0400

    🐇 added the with parameter to experiment with wider networks.

--- models/cifar10_models.py
+++ models/cifar10_models.py
@@ -3,30 +3,65 @@ import torch.nn as nn
 
 
 class VGG(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, width: int = 1):
         super(VGG, self).__init__()
+        self.width = width
         self.layers = nn.ModuleList()
 
         self.layers += [
-            nn.Sequential(nn.Conv2d(3, 16, kernel_size=3), nn.BatchNorm2d(16), nn.ReLU(inplace=True)),
-            nn.Sequential(nn.Conv2d(16, 16, kernel_size=3), nn.BatchNorm2d(16), nn.ReLU(inplace=True)),
+            nn.Sequential(
+                nn.Conv2d(3 * self.width, 16 * self.width, kernel_size=3),
+                nn.BatchNorm2d(16 * self.width),
+                nn.ReLU(inplace=True),
+            ),
+            nn.Sequential(
+                nn.Conv2d(16 * self.width, 16 * self.width, kernel_size=3),
+                nn.BatchNorm2d(16 * self.width),
+                nn.ReLU(inplace=True),
+            ),
         ]
         self.layers += [
-            nn.Sequential(nn.Conv2d(16, 32, kernel_size=3, stride=2), nn.BatchNorm2d(32), nn.ReLU(inplace=True))
+            nn.Sequential(
+                nn.Conv2d(16 * self.width, 32 * self.width, kernel_size=3, stride=2),
+                nn.BatchNorm2d(32 * self.width),
+                nn.ReLU(inplace=True),
+            )
         ]
         self.layers += [
-            nn.Sequential(nn.Conv2d(32, 32, kernel_size=3), nn.BatchNorm2d(32), nn.ReLU(inplace=True)),
-            nn.Sequential(nn.Conv2d(32, 32, kernel_size=3), nn.BatchNorm2d(32), nn.ReLU(inplace=True)),
+            nn.Sequential(
+                nn.Conv2d(32 * self.width, 32 * self.width, kernel_size=3),
+                nn.BatchNorm2d(32 * self.width),
+                nn.ReLU(inplace=True),
+            ),
+            nn.Sequential(
+                nn.Conv2d(32 * self.width, 32 * self.width, kernel_size=3),
+                nn.BatchNorm2d(32 * self.width),
+                nn.ReLU(inplace=True),
+            ),
         ]
         self.layers += [
-            nn.Sequential(nn.Conv2d(32, 64, kernel_size=3, stride=2), nn.BatchNorm2d(64), nn.ReLU(inplace=True))
+            nn.Sequential(
+                nn.Conv2d(32 * self.width, 64 * self.width, kernel_size=3, stride=2),
+                nn.BatchNorm2d(64 * self.width),
+                nn.ReLU(inplace=True),
+            )
         ]
         self.layers += [
-            nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, padding="valid"), nn.BatchNorm2d(64), nn.ReLU(inplace=True))
+            nn.Sequential(
+                nn.Conv2d(64 * self.width, 64 * self.width, kernel_size=3, padding="valid"),
+                nn.BatchNorm2d(64 * self.width),
+                nn.ReLU(inplace=True),
+            )
         ]
-        self.layers += [nn.Sequential(nn.Conv2d(64, 64, kernel_size=1), nn.BatchNorm2d(64), nn.ReLU(inplace=True))]
+        self.layers += [
+            nn.Sequential(
+                nn.Conv2d(64 * self.width, 64 * self.width, kernel_size=1),
+                nn.BatchNorm2d(64 * self.width),
+                nn.ReLU(inplace=True),
+            )
+        ]
         self.layers += [nn.AdaptiveAvgPool2d((1, 1))]
-        self.fc = nn.Linear(64, 10)
+        self.fc = nn.Linear(64 * self.width, 10)
 
     def forward(self, features: torch.Tensor, intermediate_activations_required: bool = False):
         activations = []
@@ -42,6 +77,7 @@ class VGG(torch.nn.Module):
         return features
 
 
+# This model has a kernel size of 7 for visualization purposes
 class VGG7(torch.nn.Module):
     def __init__(self):
         super(VGG7, self).__init__()
