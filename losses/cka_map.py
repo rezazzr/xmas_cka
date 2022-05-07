e3cb289a83c875f2088950808b561e86136fb1af
Reza Davari <rezazzr@hotmail.com>
2022-05-07 08:13:49 -0400

    💄 just a bit of clean up

--- losses/cka_map.py
+++ losses/cka_map.py
@@ -1,38 +1,31 @@
 from typing import List, Optional
 
-import torch
+from torch import Tensor, zeros
+from torch.nn import Module
 
 from losses.cka import TorchCKA
 
 
-class CKAMap(torch.nn.Module):
+class CKAMap(Module):
     def __init__(self):
         super(CKAMap, self).__init__()
 
-    def _make_self_map(self, activations: List[torch.Tensor]):
-        device = activations[0].device
-        batch = activations[0].shape[0]
-        cka_map = torch.zeros(len(activations), len(activations), device=device)
-        for i in range(len(activations)):
-            for j in range(len(activations)):
+    def _make_map(self, activations_1: List[Tensor], activations_2: Optional[List[Tensor]] = None) -> Tensor:
+        device = activations_1[0].device
+        batch = activations_1[0].shape[0]
+        if activations_2 is None:
+            activations_2 = activations_1
+        cka_map = zeros(len(activations_1), len(activations_2), device=device)
+        for i in range(len(activations_1)):
+            for j in range(len(activations_2)):
                 if j < i:
                     cka_map[i, j] = cka_map[j, i]
                 else:
-                    acts1 = activations[i].view(batch, -1)
-                    acts2 = activations[j].view(batch, -1)
+                    acts1 = activations_1[i].view(batch, -1)
+                    acts2 = activations_2[j].view(batch, -1)
                     cka = TorchCKA(device=device)(acts1, acts2)
                     cka_map[i, j] = cka
         return cka_map
 
-    def _make_comparison_map(self, activations: List[torch.Tensor], activations_2: List[torch.Tensor]):
-        device = activations[0].device
-        batch = activations[0].shape[0]
-        cka_map = torch.zeros(len(activations), device=device)
-        for i in range(len(activations)):
-            cka_map[i] = TorchCKA(device=device)(activations[i].view(batch, -1), activations_2[i].view(batch, -1))
-        return cka_map
-
-    def forward(self, activations: List[torch.Tensor], activations_2: Optional[List[torch.Tensor]] = None):
-        if activations_2 is None:
-            return self._make_self_map(activations)
-        return self._make_comparison_map(activations, activations_2)
+    def forward(self, activations: List[Tensor], activations_2: Optional[List[Tensor]] = None):
+        return self._make_map(activations, activations_2)
