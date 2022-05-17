bca0051b1e0fe967c08972ede3f3ba9280cc6769
Reza Davari <rezazzr@hotmail.com>
2022-05-16 22:24:37 -0400

    🍼 updated with RBF calculation version

--- losses/cka_map.py
+++ losses/cka_map.py
@@ -1,19 +1,21 @@
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
 from torch import Tensor, zeros, tensor
 from torch.nn import Module
 
-from losses.cka import BatchCKA
+from losses.cka import BatchCKA, TorchCKA
 
 
 class CKAMap(Module):
-    def __init__(self, evaluation_mode: bool = False):
+    def __init__(self, evaluation_mode: bool = False, rbf_sigma: float = -1):
         super(CKAMap, self).__init__()
+        self.rbf_sigma = rbf_sigma
         self.evaluation_mode = evaluation_mode
 
     def _make_map(self, activations_1: List[Tensor], activations_2: Optional[List[Tensor]] = None) -> Tensor:
         device = activations_1[0].device
         batch = activations_1[0].shape[0]
+        cka_function = BatchCKA(device=device) if self.rbf_sigma < 0 else TorchCKA(device=device, sigma=self.rbf_sigma)
         if activations_2 is None:
             activations_2 = activations_1
         cka_map = zeros(len(activations_1), len(activations_2), device=device)
@@ -24,7 +26,7 @@ class CKAMap(Module):
                 else:
                     acts1 = activations_1[i].view(batch, -1)
                     acts2 = activations_2[j].view(batch, -1)
-                    cka = BatchCKA(device=device)(acts1, acts2)
+                    cka = cka_function(acts1, acts2)
                     cka_map[i, j] = cka
         return cka_map
 
